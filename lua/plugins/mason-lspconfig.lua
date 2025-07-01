4750f9f3645384d392354535d1e3232d012e09d8
gerry <gerry@hellotalk.com>
2025-07-01 19:43:47 +0800

    update: opti config

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,11 +1,15 @@
 return {
     "mason-org/mason-lspconfig.nvim",
+    build = ":MasonUpdate",
     opts = {
         -- ensure_installed = {"c", "lua", "vim", "vimdoc", "python","go", "goctl", "gomod", "gosum", "gotmpl", "gowork", "java", "javascript", "json", "yaml", "xml", "powershell"},
-        ensure_installed = {"python-lsp-server", "clang-format", "luau-lsp", "gofumpt", "goimports", "golangci-lint", "golangci-lint-langserver", "gomodifytags", "gopls", "json-lsp"}
+        ensure_installed = {"python-lsp-server", "clang-format", "luau-lsp", "gofumpt", "goimports", "golangci-lint", "golangci-lint-langserver", "gomodifytags", "gopls"}
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
+        opts = {
+            
+        }
     },
 }
